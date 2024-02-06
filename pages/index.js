27bfc820368f22e3b3df84e82ad4ba60d07ff4f2
Dragos20Tech <dragospetrescu5@gmail.com>
2024-02-06 12:31:37 +0200

    Made hamburger menu work myself

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function toggleMobileNav() {
+    var navigation = document.querySelector('.primary-navigation');
+    navigation.classList.toggle('hide-mobile-nav');
+    navigation.classList.toggle('show-mobile-nav');
+}
